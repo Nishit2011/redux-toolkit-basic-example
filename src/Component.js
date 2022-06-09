16f21f5365729d9a99f52391f9251fa16d295522
Nishit2011 <nranjan.2004@gmail.com>
2022-06-09 21:34:16 +0530

    redux-toolkit example

---
+++ src/Component.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { decrement, increment, incrementByAmount } from './counter'
+
+export function Counter() {
+  const count = useSelector((state) => state.counter.value)
+  const dispatch = useDispatch()
+
+  return (
+    <div>
+      <div>
+        <button
+          aria-label="Increment value"
+          onClick={() => dispatch(increment())}
+        >
+          Increment
+        </button>
+        <span>{count}</span>
+        <button
+          aria-label="Decrement value"
+          onClick={() => dispatch(decrement())}
+        >
+          Decrement
+        </button>
+        <button   onClick={() => dispatch(incrementByAmount(5))}>Increment by 5</button>
+      </div>
+    </div>
+  )
+}
